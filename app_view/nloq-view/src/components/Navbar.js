a0f9007e900c5bc1b1fbb48e9f9cf6bb1c1a7be7
sabri-xz <sasalabrina@gmail.com>
2023-11-19 14:31:41 -0500

    added logo file

--- app_view/nloq-view/src/components/Navbar.js
+++ app_view/nloq-view/src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
+import logo from '../imgs/nloq-logo-main.png';
 
 const Navbar = () => (
   <nav>
+    <Link to="/">
+      <img src={logo} alt="Logo" className="logo" />
+    </Link>
     <ul>
       <li>
-        <Link to="/">Home</Link>
+        <Link to="/nba">Home</Link>
       </li>
-      <li>
-        <Link to="/nba">About</Link>
+      <li className="dropdown">
+        <div>Resources</div>
+        <div className="dropdown-content">
+          <Link to="/resources1">How to read our data</Link>
+          <Link to="/resources2">What is sport betting</Link>
+          <Link to="/resources3">Additional resources</Link>
+        </div>
       </li>
     </ul>
   </nav>
