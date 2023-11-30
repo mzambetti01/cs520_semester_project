d710dc95631270ed628fa97d346259be6e62c497
sabri-xz <sasalabrina@gmail.com>
2023-11-30 13:47:21 -0500

    added data formatting

--- src/frontend/src/content/data.js
+++ src/frontend/src/content/data.js
@@ -1,17 +1,25 @@
 // format data to a list of dictionaries of the actual names of fields
 const helper = (data) => {
-  if (data[0].length != 9) {
+  if (data[0].length != 10) {
     console.log("wrong data to be reformatted")
     return null
   }
-  
+  let keys = ["PlayerName", "league", "PlayerID", "ExpectedValue", 
+              "OverImpliedProb", "UnderImpliedProb", "OverAdjustedProb",
+              "UnderAdjustedProb", "OverAdjustedOdds", "UnderAdjustedOdds"];
+  return data.map(d => {
+    return keys.reduce((acc, k, i) => {
+      acc[k] = d[i];
+      return acc;
+    }, {}); 
+  })
 }
 
 // function to format data from backend
 const useProcessData = async (league) => {
   const allLeagues = ["nba", "nhl", "nfl"]
   let data;
-  let resPlayerData = [];
+  let players;
   let betData = [];
 
   // console.log(league)
@@ -21,7 +29,7 @@ const useProcessData = async (league) => {
       throw new Error('Network response was not ok');
     }
     data = await response.json();
-    resPlayerData = data.response_object; 
+    players = data.response_object.map(p => [p[0], decodeURIComponent(p[1]), league]); 
   } else {
     // list of promisses
     let responseArr = allLeagues.map(async (l) => {
@@ -30,15 +38,14 @@ const useProcessData = async (league) => {
         throw new Error('Network response was not ok');
       }
       data = await response.json();
-      return data.response_object;
+      return data.response_object.map(p => [p[0], decodeURIComponent(p[1]), l]);
     })
-    resPlayerData = await Promise.all(responseArr);
-    resPlayerData = resPlayerData.flat(1); 
+    players = await Promise.all(responseArr);
+    players = players.flat(1); 
   }
-
-  let players = resPlayerData.map(p => [p[0], decodeURIComponent(p[1])]);
   console.log(players);
 
+  // get bet data for each player
   let betDataArr = players.map(async (x) => {
     const response = await fetch(`http://localhost:5000/player_betting_data/${encodeURIComponent(x[0])}`)
     if (!response.ok) {
@@ -49,7 +56,6 @@ const useProcessData = async (league) => {
   })
 
   betData = await Promise.all(betDataArr);
-  console.log(betData.flat(1));
   const indxToKeep = [3,4,5,6,10,11,12,13]; // todo: wrong indices
   betData = betData.flat(1).map(d => indxToKeep.map(i => d[i])); // cleaning up the data array
 
@@ -57,11 +63,11 @@ const useProcessData = async (league) => {
   betData = betData.map((bet) => {
     // find the player by id in bet, concat the player info to bet info
     let p = players.find((player) => player[0] === bet[0]);
-    return [p[1], ...bet];
+    return [p[1], p[2], ...bet]; // player name, league, the rest
   })
 
   console.log(betData);
-
+  console.log(helper(betData))
 
 
   return betData;
